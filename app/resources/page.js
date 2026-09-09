c32ba40d2c6e4642dc0f1663bb5a84840fda62a3
nishu_verma <drishtiverma2608@gmail.com>
2026-09-09 13:24:06 +0530

    chore: remove resources page and links

--- app/resources/page.js
+++
@@ -1,4 +0,0 @@
-import PublicPage from '@/components/PublicPage';
-import { ResourcesPage } from '@/components/ContentPages';
-export const metadata = { title: 'Resources', description: 'Request company information, line cards and industrial product documents.' };
-export default function Page() { return <PublicPage><ResourcesPage /></PublicPage>; }
