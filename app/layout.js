c152ba5dd6a013e60508e0f9f0fe4b833a499ec0
SatiTech <www.satitechinfo@gmail.com>
2026-09-20 19:24:07 +0530

    chore: load homepage density refinements last

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ import './products-portfolio.css';
 import './product-image-fit.css';
 import './contact-project-desk.css';
 import './mobile-homepage-fixes.css';
+import './homepage-density-refinement.css';
 
 const siteUrl = 'https://satitech-official.github.io/ikinovac-global-engineering-platform';
 const gaId = process.env.NEXT_PUBLIC_GA_ID || '';
